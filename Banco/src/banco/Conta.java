42fe6a34210d1c2389eb86cb89a2430edb53d447
Anderson Souza <39925471+Souzanderson@users.noreply.github.com>
2018-06-21 17:56:53 -0300

    Primeiro programa em java
    treino de OO com java

---
+++ Banco/src/banco/Conta.java
@@ -0,0 +1,58 @@
+package banco;
+import javax.swing.*;
+
+//Classe conta
+public class Conta {
+    private double saldo;
+    private double limite;
+    private String titular;
+    
+    //construtor padrão
+    public Conta(String titular){
+        this.titular=titular;
+        saldo=0;
+        limite=0;
+    }
+    //construtor completo
+    public Conta(String titular, double saldo, double limite){
+        this.saldo=saldo;
+        this.limite=limite;
+        this.titular=titular;
+    }
+    //metodo de checar o saldo
+    void setTitular(String titular){
+        this.titular=titular;
+    }
+    //retorna o nome do titular da conta
+    String getTitular(){
+        return this.titular;
+    }
+    //retorna o saldo em conta
+    double getSaldo(){
+        return this.saldo;
+    }
+    //metodo de deposito de valores
+    void deposita(double valor){
+        this.saldo = valor;
+    }
+    //metodo de saque de valores
+    void saca(double valor){
+        JFrame frame = new JFrame("dialogo");
+        if (valor<this.saldo){
+            this.saldo-=valor;
+        }
+        else if((this.limite+this.saldo)>valor){
+            this.limite=(this.limite+this.saldo)-valor;
+            this.saldo=0;
+            JOptionPane.showMessageDialog(frame, "Valor retirado do limite!"
+                    + "\nLimite atual: "+this.limite);
+        }
+        else{
+            JOptionPane.showMessageDialog(frame, "Valor excede o limite!");
+        }
+    }
+    //retorna o limite da conta
+    double getLimite(){
+        return this.limite;
+    }
+}
